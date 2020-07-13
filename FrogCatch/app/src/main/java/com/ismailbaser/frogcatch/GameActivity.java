e49b2843dcc314f79a404656b68f6767c886ca33
Baseris-de <ismail.baser@stud.hs-hannover.de>
2020-07-13 23:14:54 +0200

    Update from Ismail

--- FrogCatch/app/src/main/java/com/ismailbaser/frogcatch/GameActivity.java
+++ FrogCatch/app/src/main/java/com/ismailbaser/frogcatch/GameActivity.java
@@ -2,13 +2,32 @@ package com.ismailbaser.frogcatch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity {
+    TextView viewname, viewscore, viewhighscore, viewtimer,viewmod;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        viewname=findViewById(R.id.viewName);
+        viewhighscore=findViewById(R.id.viewHighScore);
+        viewmod=findViewById(R.id.viewMod);
+        viewscore=findViewById(R.id.viewScore);
+        viewtimer=findViewById(R.id.viewTimer);
+        Intent intent=getIntent();
+       viewname.setText(intent.getStringExtra("userName"));
+       viewmod.setText(intent.getStringExtra("mod"));
+
+
+    }
+    public void addScore(View view){
+
+
+
     }
 }
--- FrogCatch/app/src/main/java/com/ismailbaser/frogcatch/MainActivity.java
+++ FrogCatch/app/src/main/java/com/ismailbaser/frogcatch/MainActivity.java
@@ -2,13 +2,23 @@ package com.ismailbaser.frogcatch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onplayGame(View view){
+        intent=new Intent(MainActivity.this,GameActivity.class);
+        intent.putExtra("userName","Ismail");
+        intent.putExtra("mod","Kolay");
+
+        startActivity(intent);
     }
 }
